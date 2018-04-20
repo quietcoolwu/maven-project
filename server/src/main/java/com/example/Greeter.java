ba6ea9ea6a773965d82f8801a698f3c7fcf19f57
Kai Wu <kaiw@.splunk.com>
2018-04-20 17:43:18 +0800

    add fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,15 +5,19 @@ package com.example;
  */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
+/**
+ * This is a constructor.
+ */
+public Greeter() {
 
-  }
+}
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+
+/**
+ * @param someone the name of a person
+ * @return greeting String
+ */
+public final String greet(final String someone) {
+        return String.format("Hello, %s!", someone);
+}
 }
---
+++ server/src/main/java/com/example/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * A pkg name
+ */
+
+package com.example;
